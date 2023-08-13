d843451f4bbfff78f8f340edaccc963ff488abd9
ArfazAhamed <arfazahamed31@gmail.com>
2023-08-13 21:47:14 +0600

    final touch

--- src/Home/firebase.init.js
+++ src/Home/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARGixKX7Qym8r2iIdZh0oxwgI0xL7jUnc",
-  authDomain: "popx-701ec.firebaseapp.com",
-  projectId: "popx-701ec",
-  storageBucket: "popx-701ec.appspot.com",
-  messagingSenderId: "664794898556",
-  appId: "1:664794898556:web:806c23102562888f36a9f9"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
